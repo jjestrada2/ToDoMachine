2619454dea8c2de5b3756981e2a90e8d55759419
Juan Jose Estrada <juan5801331@gmail.com>
2022-02-24 22:10:47 -0800

    add events in TodoSearch Button And IconsItem

--- src/components/CreateTodoButton.js
+++ src/components/CreateTodoButton.js
@@ -2,9 +2,15 @@ import React from 'react';
 import "./CreateTodoButton.css";
 
 function CreateTodoButton() {
+
+  const onClickCreateButton=()=>{
+    alert("Click");
+  }
+
+
   return (
     
-      <button className='CreateTodoButton'>+</button>
+      <button className='CreateTodoButton' onClick={onClickCreateButton}>+</button>
     
   )
 }
--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  
+  const onSearchValueChange=(event)=>{
+    console.log(event.target.value);
+  };
+
   return (
-    
-      <input className="TodoSearch" placeholder="Enter Task"/>
+
+      <input className="TodoSearch" placeholder="Enter Task" onChange={onSearchValueChange}/>
 
   )
 }
