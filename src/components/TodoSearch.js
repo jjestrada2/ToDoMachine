536743813158c7a15e98b364aa8c371c9ee57a98
Juan Jose Estrada <juan5801331@gmail.com>
2022-02-25 13:59:52 -0800

    Add useState toDoSearch

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -2,16 +2,27 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+//Create state to save va;ue of the input
+const[searchValue,setSearchValue]=React.useState('');
+  
   
   const onSearchValueChange=(event)=>{
     console.log(event.target.value);
+    setSearchValue(event.target.value);
   };
 
-  return (
+  return [
 
-      <input className="TodoSearch" placeholder="Enter Task" onChange={onSearchValueChange}/>
+      <input 
+      className="TodoSearch" 
+      placeholder="Enter Task" 
+      value={searchValue}
+      onChange={onSearchValueChange}/>
+  ,
 
-  )
+  <p>{searchValue}</p>
+]
+  
 }
 
 export default TodoSearch;
